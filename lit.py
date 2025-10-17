44a25feadfe17b577a7f0395bfc81baa0c8ba666
sjesun <sjesun@uwaterloo.ca>
2025-10-17 16:11:45 -0400

    Allow "git hash-object" with and without "-w"

--- lit.py
+++ lit.py
@@ -1,6 +1,20 @@
 import sys
 import os
 import zlib
+from hashlib import sha1
+
+
+"""
+compute_hash(filename): 
+    Given filename, returns its hash and all its contents for processing by 
+    'git hash-object'
+"""
+def compute_hash(filename):
+    with open(filename, "rb") as f:
+        contents = f.read()
+    whole = f"blob {len(contents)}\x00".encode("utf-8") + contents
+    object_id = sha1(whole).hexdigest()
+    return object_id, whole
 
 
 def main():
@@ -17,11 +31,12 @@ def main():
     
     # git cat-file
     elif git_command == "cat-file":
-        
-        # git cat-file -p: pretty-print
+    
         if sys.argv[2] == "-p":
+            # git cat-file -p: pretty-print
             sha_hash = sys.argv[3]
-            obj = f".git/objects/{sha_hash[0:2]}/{sha_hash[2:]}"
+            subdirectory, filename = sha_hash[:2], sha_hash[2:]
+            obj = f".git/objects/{subdirectory}/{filename}"
             with open(obj, "rb") as f:
                 rawdata = zlib.decompress(f.read())
                 
@@ -31,9 +46,31 @@ def main():
                 orig = rawcontents.decode()
                 print(orig, end="")
         
-        # other valid flags (-e, -t, -s, etc.) or invalid flags
         else:
+            # other valid flags (-e, -t, -s, etc.) or invalid flags
             raise RuntimeError(f"Unknown or unimplemented flag #{sys.argv[2]}")
+    
+    # git hash-object
+    elif git_command == "hash-object":
+        
+        if len(sys.argv) == 3:
+            # no flags provided
+            obj_id, data = compute_hash(sys.argv[2])
+        
+        elif len(sys.argv) == 4 and sys.argv[2] == "-w":
+            # git hash-object -w
+            obj_id, data = compute_hash(sys.argv[3])
+            
+            # preparing data for writing
+            compressed_data = zlib.compress(data)
+            subdirectory, file = obj_id[:2], obj_id[2:]
+            os.makedirs(f".git/objects/{subdirectory}", exist_ok=True)
+            path = f".git/objects/{subdirectory}/{file}"
+            with open(path, "wb") as f:
+                f.write(compressed_data)
+        
+        # object ID is printed either way
+        print(obj_id)
     
     # unknown or unimplemented command
     else:
